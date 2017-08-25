eb3634f1e4c87cf4585d6941d67f7f57a95258b7
Tom <tomawright95@gmail.com>
2017-08-25 08:17:27 +0100

    Added Registry file to main mod file

--- src/main/java/com/kashdeya/moreshears/main/MoreShears.java
+++ src/main/java/com/kashdeya/moreshears/main/MoreShears.java
@@ -1,5 +1,11 @@
 package com.kashdeya.moreshears.main;
 
+import com.kashdeya.moreshears.configs.Config;
+import com.kashdeya.moreshears.inits.MoreItems;
+import com.kashdeya.moreshears.proxy.CommonProxy;
+import com.kashdeya.moreshears.tabs.TabItems;
+import com.kashdeya.moreshears.util.Registry;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -10,46 +16,40 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import com.kashdeya.moreshears.configs.Config;
-import com.kashdeya.moreshears.inits.MoreItems;
-import com.kashdeya.moreshears.proxy.CommonProxy;
-import com.kashdeya.moreshears.tabs.TabItems;
-
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "after:basemetals;after:immersiveengineering")
 
 public class MoreShears {
-	
+
 	@Instance(Reference.MOD_ID)
-    public static MoreShears instance;
-	
-	@SidedProxy(clientSide=Reference.PROXY_CLIENT, serverSide=Reference.PROXY_COMMON)
+	public static MoreShears instance;
+
+	@SidedProxy(clientSide = Reference.PROXY_CLIENT, serverSide = Reference.PROXY_COMMON)
 	public static CommonProxy PROXY;
-	
+
+	public static final Registry REGISTRY = new Registry();
 	public static final CreativeTabs tabItems = new TabItems("MoreShears");
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent e) {
-    	PROXY.registerRenderers();
-    	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
 		// Configs
 		Config.initMainConfigs();
-		
+
 		// Items
 		MoreItems.init();
-		
-		// Renders
-		PROXY.registerRenderers();
-		
+		REGISTRY.preInit();
+
 		// Events
-    	MinecraftForge.EVENT_BUS.register(instance);
-    }
+		MinecraftForge.EVENT_BUS.register(instance);
+		MinecraftForge.EVENT_BUS.register(REGISTRY);
+	}
 
-    @EventHandler
-    public void init(FMLInitializationEvent e) {
-    	PROXY.init();
-    }
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+		PROXY.init();
+		REGISTRY.init();
+	}
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent e) {
-    }
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+	}
 }
