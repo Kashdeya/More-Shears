4e1e58fb2ac550bb8f540ad474089e26274b1b08
Tom <tomawright95@gmail.com>
2017-08-25 08:13:33 +0100

    Removed unnecessary RenderHandler

--- src/main/java/com/kashdeya/moreshears/main/Reference.java
+++ src/main/java/com/kashdeya/moreshears/main/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	// Mod Constants
 	public static final String MOD_ID = "moreshears";
 	public static final String MOD_NAME = "More Shears";
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "%VERSION%";
 		
 	// Proxy Constants
 	public static final String PROXY_CLIENT = "com.kashdeya.moreshears.proxy.ClientProxy";
